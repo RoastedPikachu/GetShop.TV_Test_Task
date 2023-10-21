763c0eab4934fb4be5af52dcb84c80ace856e20a
Boris Karabut <boriskarabytwork@gmail.com>
2023-10-21 17:05:12 +0300

    Релизовал валидацию номера телефона и с маской и без

--- src/widgets/shared/banners/ThePhoneFormBanner.tsx
+++ src/widgets/shared/banners/ThePhoneFormBanner.tsx
@@ -1,16 +1,46 @@
 "use client";
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import InputMask from "react-input-mask";
+import axios from "axios";
 
 import NumPadButton from "@/widgets/shared/buttons/NumPadButton";
 
 const ThePhoneFormBanner = () => {
-  const [phoneNumber, setPhoneNumber] = useState("");
+  let [phoneNumber, setPhoneNumber] = useState("");
   const [isConfirmed, setIsConfirmed] = useState(false);
-  const [isPhoneVerified, setIsPhoneVerified] = useState(false);
+  const [isPhoneInvalid, setIsPhoneInvalid] = useState(false);
+  const [isResponseReceived, setIsResponseReceived] = useState(false);
+
+  useEffect(() => {
+    let comparedVariable: string = phoneNumber.replace(/[\(\)_\-\+]/g, "");
+
+    if (comparedVariable[0] === "7") {
+      comparedVariable = comparedVariable.slice(1);
+    }
+
+    if (comparedVariable.length === 10) {
+      phoneNumber = comparedVariable;
+
+      axios
+        .post(
+          `http://apilayer.net/api/validate?access_key=45f87171678e25604922fe59302e5dfd&number=${phoneNumber}&country_code=RU`,
+        )
+        .then((res: any) => {
+          console.log(res);
+          setIsPhoneInvalid(!res.data.valid);
+          setIsResponseReceived(true);
+        });
+    }
+  }, [phoneNumber]);
 
   const addNumberToPhoneNumber = (number: string) => {
     setPhoneNumber(phoneNumber + number);
+  };
+
+  const clearPhoneNumber = () => {
+    setPhoneNumber("");
+    setIsPhoneInvalid(false);
+    setIsResponseReceived(false);
   };
 
   return (
@@ -25,7 +55,9 @@ const ThePhoneFormBanner = () => {
         alwaysShowMask={true}
         onChange={(event) => setPhoneNumber(event.target.value)}
         value={phoneNumber}
-        className="w-[100%] h-[40px] bg-transparent border-none text-[2rem] text-center outline-none"
+        className={`w-[100%] h-[40px] bg-transparent border-none ${
+          isPhoneInvalid && isResponseReceived ? "text-[#ea0000]" : ""
+        } text-[2rem] text-center outline-none`}
       />
 
       <p className="mt-[10px] w-full text-[0.875rem] leading-[17px]">
@@ -52,7 +84,7 @@ const ThePhoneFormBanner = () => {
         <NumPadButton number={9} callback={addNumberToPhoneNumber} />
 
         <button
-          onClick={() => setPhoneNumber("")}
+          onClick={() => clearPhoneNumber()}
           className="w-[185px] h-[50px] hover:bg-[#000000] focus:bg-[#000000] border-[#000000] border-2 hover:text-[#ffffff] focus:text-[#ffffff] font-medium duration-[400ms] ease-in-out"
         >
           Стереть
@@ -61,7 +93,9 @@ const ThePhoneFormBanner = () => {
         <NumPadButton number={0} callback={addNumberToPhoneNumber} />
       </div>
 
-      {isPhoneVerified || phoneNumber.length < 10 ? (
+      {(!isPhoneInvalid && isResponseReceived) ||
+      (phoneNumber.replace(/[\(\)_\-\+]/g, "").length < 11 &&
+        !isResponseReceived) ? (
         <div className="flex justify-between items-center mt-[30px] w-full h-[50px]">
           <input
             type="checkbox"
@@ -92,7 +126,7 @@ const ThePhoneFormBanner = () => {
 
       <button
         className={`mt-[20px] w-full h-[50px] border-[1px] ${
-          isConfirmed && isPhoneVerified
+          isConfirmed && !isPhoneInvalid && isResponseReceived
             ? "bg-[#000000] border-[#000000] text-[#ffffff]"
             : "border-[#4e4e4e] text-[#4e4e4e]"
         } text-[1rem] font-medium`}
